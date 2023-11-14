589c01ba6fccdeb278a9383bd5a9529add1fd58f
Maria Panaiba <mariaeduardalopesparnaiba@gmail.com>
2023-11-14 11:19:24 -0300

    atualização das variaveis

--- studentManagement/models.py
+++ studentManagement/models.py
@@ -7,3 +7,9 @@ class Student(models.Model):
     Classe responsável pelo cadastro de alunos
     """
     name = models.CharField(max_length=50, blank=False)
+    matricula = models.CharField(max_length=50, blank=False, default="0")
+    genero = models.CharField(max_length=10, blank=False, default="neutre")
+    idade = models.CharField(max_length=3, blank=False, default="0")
+    turma = models.CharField(max_length=50, blank=False, default="0")
+    email = models.CharField(max_length=70, blank=False, default="gmail")
+
